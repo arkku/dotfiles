534090a32a170e8243f9ccd6643aa04213c92dfd
Kimmo Kulovesi <kk@arkku.com>
2026-05-18 03:09:16 +0300

    Remove the deprecated hl_on_yank, reset terminal between sshell sessions

--- .config/nvim/after/plugin/arkku_settings.lua
+++ .config/nvim/after/plugin/arkku_settings.lua
@@ -35,16 +35,6 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end,
 })
 
--- Highlight yanked text momentarily
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
-    desc = 'Hightlight selection on yank',
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
-    end,
-})
-
 -- Breakpoint sign
 vim.fn.sign_define("DapBreakpoint", {
     text = "●",
